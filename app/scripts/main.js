27d52590e7931682864a5483b53fbdceaae96d2b
Gauthier Eloy <gauthier@igloo.be>
2014-04-30 18:00:37 +0200

    Side view
    Signed-off-by: Gauthier Eloy <gauthier@igloo.be>

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
   $('.btn-shortcut-menu').click(function(e) {
+    e.preventDefault();
     $('.workspace').toggleClass('menu-open');
+    if($('.workspace').hasClass('side-view-open')) {
+      $('.workspace').removeClass('menu-open side-view-open');
+    }
   });
+
+  $('.nav-item a').click(function(e) {
+    e.preventDefault();
+    $('.workspace').toggleClass('side-view-open');
+  });
+
 });
